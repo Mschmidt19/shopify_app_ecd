89b0a2887ce6655c02096b2bab5d3c93215cf994
Marek <MarekSchmidt@outlook.com>
2018-10-15 16:55:23 -0700

    removed @product definition in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,5 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 20 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
-    @product = ShopifyAPI::Product.new
   end
 end
