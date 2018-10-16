5fb2f9153603b7594b7411679243047f1683655d
Marek <MarekSchmidt@outlook.com>
2018-10-15 18:49:34 -0700

    First try on generate all, price attempt v2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,6 @@ class ProductsController < ShopifyApp::AuthenticatedController
     @product = ShopifyAPI::Product.new
     @product.title = params[:title]
     @product.product_type = params[:product_type]
-    @product.price = params[:price]
     sizes = params[:sizes].split(/\s*,\s*/)
     colors = params[:colors].split(/\s*,\s*/)
     variants = []
@@ -13,6 +12,7 @@ class ProductsController < ShopifyApp::AuthenticatedController
         variant = {}
         variant["option1"] = size
         variant["option2"] = color
+        variant["price"] = params[:price]
         variants.push(variant)
       end
     end
@@ -35,7 +35,97 @@ class ProductsController < ShopifyApp::AuthenticatedController
   end
 
   def generateall
-
+    products = [
+      {"title": "Short Sleeve Crewneck",
+        "product_type": "Tops",
+        "sizes": "S, M, L",
+        "colors": "White, Black, Gray, Blue",
+        "price": "15.00"
+      },
+      {"title": "Short Sleeve V-Neck",
+        "product_type": "Tops",
+        "sizes": "S, M, L",
+        "colors": "White, Black, Gray, Blue",
+        "price": "15.00"
+      },
+      {"title": "Short Sleeve Henley",
+        "product_type": "Tops",
+        "sizes": "S, M, L",
+        "colors": "White, Black, Gray, Blue",
+        "price": "15.00"
+      },
+      {"title": "Long Sleeve Crewneck",
+        "product_type": "Tops",
+        "sizes": "S, M, L",
+        "colors": "White, Black, Gray, Blue",
+        "price": "20.00"
+      },
+      {"title": "Long Sleeve V-Neck",
+        "product_type": "Tops",
+        "sizes": "S, M, L",
+        "colors": "White, Black, Gray, Blue",
+        "price": "20.00"
+      },
+      {"title": "Long Sleeve Henley",
+        "product_type": "Tops",
+        "sizes": "S, M, L",
+        "colors": "White, Black, Gray, Blue",
+        "price": "20.00"
+      },
+      {"title": "Khaki Shorts",
+        "product_type": "Bottoms",
+        "sizes": "30, 32, 34, 36, 38, 40",
+        "colors": "Blue, Black, Brown",
+        "price": "20.00"
+      },
+      {"title": "Denim Shorts",
+        "product_type": "Bottoms",
+        "sizes": "30, 32, 34, 36, 38, 40",
+        "colors": "Blue, Black, Brown",
+        "price": "20.00"
+      },
+      {"title": "Khaki Pants",
+        "product_type": "Bottoms",
+        "sizes": "30, 32, 34, 36, 38, 40",
+        "colors": "Blue, Black, Brown",
+        "price": "30.00"
+      },
+      {"title": "Denim Pants",
+        "product_type": "Bottoms",
+        "sizes": "30, 32, 34, 36, 38, 40",
+        "colors": "Blue, Black, Brown",
+        "price": "30.00"
+      }
+    ]
+    products.each do |product|
+      new_product = ShopifyAPI::Product.new
+      new_product.title = product["title"]
+      new_product.product_type = product["product_type"]
+      sizes = products["sizes"].split(/\s*,\s*/)
+      colors = products["colors"].split(/\s*,\s*/)
+      variants = []
+      sizes.each do |size|
+        colors.each do |color|
+          variant = {}
+          variant["option1"] = size
+          variant["option2"] = color
+          variant["price"] = params[:price]
+          variants.push(variant)
+        end
+      end
+      new_product.variants = variants
+      new_product.options = [
+        {
+          "name": "Size",
+          "values": sizes
+        },
+        {
+          "name": "Color",
+          "values": colors
+        }
+      ]
+      new_product.save
+    end
   end
 
   def deleteall
