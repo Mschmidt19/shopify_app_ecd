468ad3921d99bea9886890a1735da63db0b75660
Marek <MarekSchmidt@outlook.com>
2018-10-15 18:28:52 -0700

    Added price field, trying enterable variants

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,20 +4,31 @@ class ProductsController < ShopifyApp::AuthenticatedController
     @product = ShopifyAPI::Product.new
     @product.title = params[:title]
     @product.product_type = params[:product_type]
-    # sizes = params[:sizes].split(/\s*,\s*/)
-    # colors = params[:colors].split(/\s*,\s*/)
-    # @product.options = [
-    #   {
-    #     "name": "Size",
-    #     "values": sizes
-    #   },
-    #   {
-    #     "name": "Color",
-    #     "values": colors
-    #   }
-    # ]
-    @product.variants = [{"option1": "Blue", "option2": "155"}, {"option1": "Black", "option2": "159"}]
-    @product.options = [{"name": "Color", "values": ["Blue", "Black"]}, {"name": "Size", "values": ["155", "159"]}]
+    @product.price = params[:price]
+    sizes = params[:sizes].split(/\s*,\s*/)
+    colors = params[:colors].split(/\s*,\s*/)
+    variants = []
+    sizes.each do |size|
+      colors.each do |color|
+        variant = {}
+        variant["option1"] = size
+        variant["option2"] = color
+        variants.push(variant)
+      end
+    end
+    @product.variants = variants
+    @product.options = [
+      {
+        "name": "Size",
+        "values": sizes
+      },
+      {
+        "name": "Color",
+        "values": colors
+      }
+    ]
+    # @product.variants = [{"option1": "Blue", "option2": "155"}, {"option1": "Black", "option2": "159"}]
+    # @product.options = [{"name": "Color", "values": ["Blue", "Black"]}, {"name": "Size", "values": ["155", "159"]}]
     if @product.save
       redirect_to root_path
     end
