dba45173ef3bf33427c04c194d401349d1501210
Marek <MarekSchmidt@outlook.com>
2018-10-15 17:01:52 -0700

    Attempt 4

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 20 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
+    p "Made it to home page"
   end
 end
