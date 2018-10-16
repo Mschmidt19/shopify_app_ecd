6e337886aa1f57ed3f76585f0fe14afa1e0345f6
Marek <MarekSchmidt@outlook.com>
2018-10-15 18:11:59 -0700

    Trying hardcoded variants

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,18 +4,20 @@ class ProductsController < ShopifyApp::AuthenticatedController
     @product = ShopifyAPI::Product.new
     @product.title = params[:title]
     @product.product_type = params[:product_type]
-    sizes = params[:sizes].split(/\s*,\s*/)
-    colors = params[:colors].split(/\s*,\s*/)
-    @product.options = [
-      {
-        "name": "Size",
-        "values": sizes
-      },
-      {
-        "name": "Color",
-        "values": colors
-      }
-    ]
+    # sizes = params[:sizes].split(/\s*,\s*/)
+    # colors = params[:colors].split(/\s*,\s*/)
+    # @product.options = [
+    #   {
+    #     "name": "Size",
+    #     "values": sizes
+    #   },
+    #   {
+    #     "name": "Color",
+    #     "values": colors
+    #   }
+    # ]
+    @product.variants = [{"option1": "Blue", "option2": "155"}, {"option1": "Black", "option2": "159"}]
+    @product.options = [{"name": "Color", "values": ["Blue", "Black"]}, {"name": "Size", "values": ["155", "159"]}]
     if @product.save
       redirect_to root_path
     end
@@ -26,9 +28,8 @@ class ProductsController < ShopifyApp::AuthenticatedController
   end
 
   def deleteall
-    ShopifyAPI::product.find(10).destroy
+    ShopifyAPI::Product.find(10).destroy
     redirect_to root_path
-
   end
 
 end
