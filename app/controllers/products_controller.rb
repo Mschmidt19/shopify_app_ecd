0608d233c08565b5eea471f96b908e6dc456b122
Marek <MarekSchmidt@outlook.com>
2018-10-15 17:56:43 -0700

    Adjusting delete to only item 11 to try, adding variants to add

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,18 @@ class ProductsController < ShopifyApp::AuthenticatedController
     @product = ShopifyAPI::Product.new
     @product.title = params[:title]
     @product.product_type = params[:product_type]
+    sizes = params[:sizes].split(/\s*,\s*/)
+    colors = params[:colors].split(/\s*,\s*/)
+    @product.options = [
+      {
+        "name": "Size",
+        "values": sizes
+      },
+      {
+        "name": "Color",
+        "values": colors
+      }
+    ]
     if @product.save
       redirect_to root_path
     end
@@ -14,7 +26,8 @@ class ProductsController < ShopifyApp::AuthenticatedController
   end
 
   def deleteall
-    @products = ShopifyAPI::product.destroy(:all)
+    ShopifyAPI::product.find(10).destroy
+    redirect_to root_path
 
   end
 
