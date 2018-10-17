9acdd42877437eedc17853d6f979de6a9d779c29
Marek <MarekSchmidt@outlook.com>
2018-10-16 22:21:46 -0700

    added proper commas

--- db/migrate/20181017051604_change_integer_limit_in_variants.rb
+++ db/migrate/20181017051604_change_integer_limit_in_variants.rb
@@ -1,6 +1,6 @@
 class ChangeIntegerLimitInVariants < ActiveRecord::Migration[5.2]
   def change
-    change_column :variants, :shopify_id :integer, limit: 8
-    change_column :variants, :shopify_product_id :integer, limit: 8
+    change_column :variants, :shopify_id, :integer, limit: 8
+    change_column :variants, :shopify_product_id, :integer, limit: 8
   end
 end
