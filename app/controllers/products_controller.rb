6747499d8d16e9a3bae5db279a109761ba736732
Marek <MarekSchmidt@outlook.com>
2018-10-17 17:57:38 -0700

    formatting spacing after each rails product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -67,29 +67,7 @@ class ProductsController < ShopifyApp::AuthenticatedController
           else
             rails_product = Product.where(shopify_id: new_product.id).first
           end
-          variant_hash_array = []
-          new_product.variants.each do |variant|
-            if !Variant.exists?(shopify_product_id: new_product.id, shopify_id: variant.id)
-              variant_hash = {
-                "shopify_id": variant.id,
-                "shopify_product_id": variant.product_id,
-                "title": variant.title,
-                "shopify_created_at": variant.created_at,
-                "shopify_updated_at": variant.updated_at,
-                "price": variant.price,
-                "sku": variant.sku,
-                "option1": variant.option1,
-                "option2": variant.option2,
-                "option3": variant.option3,
-                "position": variant.position,
-                "taxable": variant.taxable,
-                "weight": variant.weight,
-                "weight_unit": variant.weight_unit,
-                "inventory_quantity": variant.inventory_quantity
-              }
-              variant_hash_array.push(variant_hash)
-            end
-          end
+          variant_hash_array = create_new_variants_array
           rails_product.variants.create(variant_hash_array) unless variant_hash_array.empty?
         end
         puts `Processing page #{page} of #{total_pages}`
@@ -100,71 +78,34 @@ class ProductsController < ShopifyApp::AuthenticatedController
     end
   end
 
-  # def save_one_to_database
-  #   new_product = ShopifyAPI::Product.all(:params => {:limit => 1}).first
-  #   hash = {
-  #     "shopify_id": new_product.id,
-  #     "title": new_product.title,
-  #     "body_html": new_product.body_html,
-  #     "vendor": new_product.vendor,
-  #     "product_type": new_product.product_type,
-  #     "shopify_created_at": new_product.created_at,
-  #     "handle": new_product.handle,
-  #     "shopify_updated_at": new_product.updated_at,
-  #     "shopify_published_at": new_product.published_at,
-  #     "tags": new_product.tags
-  #   }
-  #
-  #   variant_hash_array = []
-  #   new_product.variants.each do |variant|
-  #     variant_hash = {
-  #       "shopify_id": variant.id,
-  #       "shopify_product_id": variant.product_id,
-  #       "title": variant.title,
-  #       "shopify_created_at": variant.created_at,
-  #       "shopify_updated_at": variant.updated_at,
-  #       "price": variant.price,
-  #       "sku": variant.sku,
-  #       "option1": variant.option1,
-  #       "option2": variant.option2,
-  #       "option3": variant.option3,
-  #       "position": variant.position,
-  #       "taxable": variant.taxable,
-  #       "weight": variant.weight,
-  #       "weight_unit": variant.weight_unit,
-  #       "inventory_quantity": variant.inventory_quantity
-  #     }
-  #     variant_hash_array.push(variant_hash)
-  #   end
-  #
-  #   rails_product = Product.create(hash)
-  #   rails_product.variants.create(variant_hash_array)
-  #   redirect_to root_path
-  # end
-  #
-  # def save_five_to_database
-  #   products = ShopifyAPI::Product.all(:params => {:page => 2, :limit => 5})
-  #   hashes = []
-  #   products.each do |new_product|
-  #     hash = {
-  #       "shopify_id": new_product.id,
-  #       "title": new_product.title,
-  #       "body_html": new_product.body_html,
-  #       "vendor": new_product.vendor,
-  #       "product_type": new_product.product_type,
-  #       "shopify_created_at": new_product.created_at,
-  #       "handle": new_product.handle,
-  #       "shopify_updated_at": new_product.updated_at,
-  #       "shopify_published_at": new_product.published_at,
-  #       "tags": new_product.tags
-  #     }
-  #     hashes.push(hash)
-  #   end
-  #   rails_product = Product.create(hashes)
-  #   redirect_to root_path
-  # end
-
   private
+
+  def create_new_variants_array
+    variant_hash_array = []
+    new_product.variants.each do |variant|
+      if !Variant.exists?(shopify_product_id: new_product.id, shopify_id: variant.id)
+        variant_hash = {
+          "shopify_id": variant.id,
+          "shopify_product_id": variant.product_id,
+          "title": variant.title,
+          "shopify_created_at": variant.created_at,
+          "shopify_updated_at": variant.updated_at,
+          "price": variant.price,
+          "sku": variant.sku,
+          "option1": variant.option1,
+          "option2": variant.option2,
+          "option3": variant.option3,
+          "position": variant.position,
+          "taxable": variant.taxable,
+          "weight": variant.weight,
+          "weight_unit": variant.weight_unit,
+          "inventory_quantity": variant.inventory_quantity
+        }
+        variant_hash_array.push(variant_hash)
+      end
+    end
+    return variant_hash_array
+  end
 
   def product_params
     params.require(:product).permit(:shopify_id, :title, :shopify_created_at, :shopify_updated_at, :shopify_published_at, :body_html, :handle, :product_type, :tags, :vendor)
